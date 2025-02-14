360040d4412fd940ec2c80c836e68dcb6fccceb6
binarywz <binarywz@163.com>
2025-02-14 15:24:01 +0800

    feat:删除无用文件

--- process_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-if __name__ == '__main__':
-    weather_data = pd.read_table("aquacrop/data/810018.txt")
-    weather_data.to_excel("临淄区.xlsx", index=False)
-    data = pd.read_excel("临淄区.xlsx")
-    data.to_csv('weather.txt', sep='\t', index=False)
